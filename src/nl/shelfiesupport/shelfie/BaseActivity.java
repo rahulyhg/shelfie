ba7a0e2f3a628bac9f0880f51cf0bcbb9e31b7ed
Rene van der Ark <r.van.der.ark@gmail.com>
2014-07-31 21:24:50 +0200

    moved export action to main activity; added localization

--- src/nl/shelfiesupport/shelfie/BaseActivity.java
+++ src/nl/shelfiesupport/shelfie/BaseActivity.java
@@ -4,19 +4,15 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
-import org.json.JSONException;
-import org.json.JSONObject;
 
-public class BaseActivity extends Activity implements Responder {
+
+public class BaseActivity extends Activity  {
     protected Shelf shelf;
     protected GroceryList groceryList;
-    private View exportButton = null;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -91,44 +87,5 @@ public class BaseActivity extends Activity implements Responder {
         overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
     }
 
-    @SuppressWarnings("unused")
-    public void startShelfExport(View view) {
-        if(shelf == null) { shelf = Shelf.getInstance(this); }
-        Toast.makeText(this, getString(R.string.exporting), Toast.LENGTH_LONG).show();
-        exportButton = view;
-        exportButton.setEnabled(false);
-        exportButton.setBackgroundColor(getResources().getColor(R.color.shelfie_darker_blue));
 
-
-        new ExportTask(this, shelf).execute("");
-    }
-
-    private void parseAndShareExport(String jsonStr) {
-        try {
-            String id = ((JSONObject) new JSONObject(jsonStr).getJSONArray("added").get(0)).getString("_id");
-            Log.d("SHELFIE", id);
-            Intent shareIntent = new Intent(android.content.Intent.ACTION_SEND);
-            shareIntent.setType("text/plain");
-            shareIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, getString(R.string.share_subject));
-            shareIntent.putExtra(android.content.Intent.EXTRA_TEXT, getString(R.string.share_subject) + " " +
-                    getString(R.string.via) + ": http://getshelfie.herokuapp.com/" + id);
-            startActivity(Intent.createChooser(shareIntent, getString(R.string.share_title)));
-        } catch (JSONException e) {
-            Log.w("SHELFIE", "failed to parse response: " + jsonStr);
-            Toast.makeText(this, getString(R.string.export_failed), Toast.LENGTH_LONG).show();
-        }
-    }
-
-    @Override
-    public void respondWith(String response) {
-        if(exportButton != null) {
-            exportButton.setEnabled(true);
-            exportButton.setBackgroundColor(getResources().getColor(R.color.shelfie_blue));
-        }
-        if(response != null) {
-            parseAndShareExport(response);
-        } else {
-            Toast.makeText(this, getString(R.string.export_failed), Toast.LENGTH_LONG).show();
-        }
-    }
 }
--- src/nl/shelfiesupport/shelfie/MainActivity.java
+++ src/nl/shelfiesupport/shelfie/MainActivity.java
@@ -1,9 +1,16 @@
 package nl.shelfiesupport.shelfie;
 
 import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-public class MainActivity extends BaseActivity  {
+public class MainActivity extends BaseActivity implements Responder {
+    private View exportButton = null;
 
     @Override
     public void onResume() {
@@ -30,5 +37,45 @@ public class MainActivity extends BaseActivity  {
 
     }
 
+    @SuppressWarnings("unused")
+    public void startShelfExport(View view) {
+        if(shelf == null) { shelf = Shelf.getInstance(this); }
+        Toast.makeText(this, getString(R.string.exporting), Toast.LENGTH_LONG).show();
+        exportButton = view;
+        exportButton.setEnabled(false);
+        exportButton.setBackgroundColor(getResources().getColor(R.color.shelfie_darker_blue));
+
+
+        new ExportTask(this, shelf).execute("");
+    }
+
+    private void parseAndShareExport(String jsonStr) {
+        try {
+            String id = ((JSONObject) new JSONObject(jsonStr).getJSONArray("added").get(0)).getString("_id");
+            Log.d("SHELFIE", id);
+            Intent shareIntent = new Intent(android.content.Intent.ACTION_SEND);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, getString(R.string.share_subject));
+            shareIntent.putExtra(android.content.Intent.EXTRA_TEXT, getString(R.string.share_subject) + " " +
+                    getString(R.string.via) + ": http://getshelfie.herokuapp.com/" + id);
+            startActivity(Intent.createChooser(shareIntent, getString(R.string.share_title)));
+        } catch (JSONException e) {
+            Log.w("SHELFIE", "failed to parse response: " + jsonStr);
+            Toast.makeText(this, getString(R.string.export_failed), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void respondWith(String response) {
+        if(exportButton != null) {
+            exportButton.setEnabled(true);
+            exportButton.setBackgroundColor(getResources().getColor(R.color.shelfie_blue));
+        }
+        if(response != null) {
+            parseAndShareExport(response);
+        } else {
+            Toast.makeText(this, getString(R.string.export_failed), Toast.LENGTH_LONG).show();
+        }
+    }
 
 }
