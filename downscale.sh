93c0d7d0b2baf361b5318eac0857611b85ba9bf9
rar011 <r.van.der.ark@gmail.com>
2014-08-05 14:24:55 +0200

    iconography

---
+++ downscale.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+infile=res/drawable-xxxhdpi/$1
+inW=$(identify -ping -format '%W' ${infile})
+inH=$(identify -ping -format '%H' ${infile})
+
+xxhdpiW=$(bc <<< $inW*0.75)
+xxhdpiH=$(bc <<< $inH*0.75)
+
+xhdpiW=$(bc <<< $inW*0.5)
+xhdpiH=$(bc <<< $inH*0.5)
+
+hdpiW=$(bc <<< $inW*0.375)
+hdpiH=$(bc <<< $inH*0.375)
+
+mdpiW=$(bc <<< $inW*0.25)
+mdpiH=$(bc <<< $inH*0.25)
+
+convert -resize $(printf '%.0f' ${xxhdpiW})x$(printf '%.0f' ${xxhdpiH})  res/drawable-xxxhdpi/$1 res/drawable-xxhdpi/$1
+
+convert -resize $(printf '%.0f' ${xhdpiW})x$(printf '%.0f' ${xhdpiH})  res/drawable-xxxhdpi/$1 res/drawable-xhdpi/$1
+
+convert -resize $(printf '%.0f' ${hdpiW})x$(printf '%.0f' ${hdpiH})  res/drawable-xxxhdpi/$1 res/drawable-hdpi/$1
+
+convert -resize $(printf '%.0f' ${mdpiW})x$(printf '%.0f' ${mdpiH})  res/drawable-xxxhdpi/$1 res/drawable-mdpi/$1
